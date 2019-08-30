cc084e6c21227a4e1074d95218899dd189c8db8d
andrewhall <hallal17@wfu.edu>
2019-08-30 11:04:43 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/project1_hallal17.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project1_hallal17.dir/main.cpp.o"
+  "project1_hallal17.pdb"
+  "project1_hallal17"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project1_hallal17.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
